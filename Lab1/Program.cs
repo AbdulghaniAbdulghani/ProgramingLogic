3b922027a5dc5152d613bd1ce1e5bb767c3a498a
AbdulghaniAbdulghani <carlosabdulghani11@gmail.com>
2025-03-25 01:06:53 +0000

    Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to iT-1050!");
+    }
+}
