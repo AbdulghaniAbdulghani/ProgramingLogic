fb147b3aeef8000099a5bb8c01ff2fcd85966b7a
AbdulghaniAbdulghani <carlosabdulghani11@gmail.com>
2025-03-24 22:16:25 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,9 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        String name = "Abdul";
-
-        Console.WriteLine("Hello, " + name + "!");
-        Console.WriteLine("Welcome to iT-1050!");
+        // Step 1
+        Console.WriteLine("Abdulghani");
+        Console.WriteLine("Programing Logic iT-1050!");
+        // Step 2
+        /*[Abdulghani] [iT-1050-Lab1]*/
+        // Step 3
+        int myNum = 7;
+        Console.WriteLine(myNum);("My Favorite Number is" + myNum + "7");
+        // Step 4
+        string name = "JavaScript";
+        Console.WriteLine(name);
+        
+        
+       
+        
     }
 }
