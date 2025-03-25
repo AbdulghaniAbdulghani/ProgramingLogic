35511c4ebfff6d129304a518c56b803eaf8bfa20
AbdulghaniAbdulghani <carlosabdulghani11@gmail.com>
2025-03-25 01:17:17 +0000

    Modified Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        String name = "Abdul";
+
+        Console.WriteLine("Hello, " + name + "!");
         Console.WriteLine("Welcome to iT-1050!");
     }
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3ca26bf0b32fa0264fa308b4950a4c8d314b10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b922027a5dc5152d613bd1ce1e5bb767c3a498a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
